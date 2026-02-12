6a84cfdc9e87ae6d93d130f5531d19347f550511
junsang-ee <dkwnstkddk@gmail.com>
2026-02-12 12:42:11 +0900

    feat: #2 SpringExceptionHandler 정의 (Spring 기본 예외 처리 핸들러) SystemExceptionHandler 정의 (프로젝트 내 커스텀 Business 에러 등) System, Spring @Order 우선순위 정의 ErrorCode 기본 에러 추가 정의 Api 공통 응답규격 정의로 인한 BaseExcpetionHandler 구현

--- src/main/java/com/goti/constants/messages/ErrorCode.java
+++ src/main/java/com/goti/constants/messages/ErrorCode.java
@@ -12,8 +12,15 @@ import java.text.MessageFormat;
 public enum ErrorCode {
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
 	INVALID_DOMAIN_FIELD(HttpStatus.BAD_REQUEST, "도메인 필드 오류 : {0}"),
-	BODY_FIELD_ERROR(HttpStatus.BAD_REQUEST, "바디 필드 오류 : {0}")
+	BODY_FIELD_ERROR(HttpStatus.BAD_REQUEST, "바디 필드 오류 : {0}"),
+
+	TYPE_MISMATCH(HttpStatus.BAD_REQUEST, "{0} 타입 오류"),
+	MISSING_PARAMETER(HttpStatus.BAD_REQUEST, "{0} 파라미터 필요"),
+	INVALID_FORMAT(HttpStatus.BAD_REQUEST, "{0} 형식 오류"),
+
+	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부 오류가 발생하였습니다. 잠시 후 다시 시도해주세요.")
 	;
+
 
 	private final HttpStatus status;
 	private final String message;
--- src/main/java/com/goti/exception/handler/base/BaseExceptionHandler.java
+++ src/main/java/com/goti/exception/handler/base/BaseExceptionHandler.java
@@ -1,9 +1,37 @@
 package com.goti.exception.handler.base;
 
+import com.goti.common.api.ApiErrorResponse;
+
+import com.goti.constants.messages.ErrorCode;
+import com.goti.exception.BaseException;
+
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @Slf4j
 public class BaseExceptionHandler {
-	// todo: ApiErrorResponse 구현 (공통 API 규격 정의시)
+	public ResponseEntity<ApiErrorResponse> toResponse(BaseException ex) {
+		HttpStatus status = ex.error().getStatus();
+		String message = ex.getMessage();
+		return toResponse(status, message);
+	}
+
+	public ResponseEntity<ApiErrorResponse> toResponse(ErrorCode errorCode) {
+		HttpStatus status = errorCode.getStatus();
+		String message = errorCode.getMessage();
+		return toResponse(status, message);
+	}
+
+	public ResponseEntity<ApiErrorResponse> toResponse(ErrorCode errorCode, Object... args) {
+		HttpStatus status = errorCode.getStatus();
+		String message = errorCode.format(args);
+		return toResponse(status, message);
+	}
+
+	public ResponseEntity<ApiErrorResponse> toResponse(HttpStatus status, String message) {
+		return ApiErrorResponse.error(status, message);
+	}
 
 }
