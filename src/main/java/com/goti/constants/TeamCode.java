a0d4e80259f1ccf84029ccc62b85141f127eb9dc
Josuyeon89 <138758347+Josuyeon89@users.noreply.github.com>
2026-02-12 16:39:17 +0900

    [FEAT] baseball_team Entity 구현 (#13)
    * feat:
    
    team_code enum 추가
    
    * feat:
    
    baseball_team Entity 작성
    
    * refactor:
    
    length 제거
    
    * refactor:
    
    teamCode UNIQUE 제약을 @Table 레벨로 변경
    
    * refactor:
    
    생성자를 private으로 변경하고 static factory 메서드 추가

---
+++ src/main/java/com/goti/constants/TeamCode.java
@@ -0,0 +1,14 @@
+package com.goti.constants;
+
+public enum TeamCode {
+	LG,
+	DO,
+	KIW,
+	HH,
+	KT,
+	SSG,
+	KIA,
+	LOT,
+	NC,
+	SS
+}
---
+++ src/main/java/com/goti/domain/entity/baseball/BaseballTeamEntity.java
@@ -0,0 +1,135 @@
+package com.goti.domain.entity.baseball;
+
+import com.goti.constants.TeamCode;
+
+import com.goti.domain.base.BaseUuidEntity;
+
+import com.goti.domain.base.ModificationTimestampEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@Entity
+@Table(
+	name = "baseball_team",
+	uniqueConstraints = {
+		@UniqueConstraint(name = "uk_baseball_team_team_code", columnNames = {"team_code"})
+	})
+@NoArgsConstructor(access = PROTECTED)
+public class BaseballTeamEntity extends ModificationTimestampEntity {
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private TeamCode teamCode;
+
+	@Column(nullable = false)
+	private String teamName;
+
+	@Column(nullable = false)
+	private String teamNameEn;
+
+	@Column(nullable = false)
+	private String sponsor;
+
+	@Column(nullable = false)
+	private String homeGround;
+
+	@Column(nullable = false)
+	private Integer foundedYear;
+
+	@Column(nullable = false)
+	private String officeAddress;
+
+	@Column(nullable = false)
+	private String zipCode;
+
+	@Column(nullable = false)
+	private String siteAddress;
+
+	@Column(nullable = false)
+	private String owner;
+
+	@Column
+	private String ownerAgency;
+
+	@Column
+	private String ceo;
+
+	@Column(nullable = false)
+	private String generalManager;
+
+	@Column(nullable = false)
+	private String director;
+
+	@Column
+	private String logoUrl;
+
+	private BaseballTeamEntity(
+		TeamCode teamCode,
+		String teamName,
+		String teamNameEn,
+		String sponsor,
+		String homeGround,
+		Integer foundedYear,
+		String officeAddress,
+		String zipCode,
+		String siteAddress,
+		String owner,
+		String ownerAgency,
+		String ceo,
+		String generalManager,
+		String director,
+		String logoUrl
+	) {
+		this.teamCode = teamCode;
+		this.teamName = teamName;
+		this.teamNameEn = teamNameEn;
+		this.sponsor = sponsor;
+		this.homeGround = homeGround;
+		this.foundedYear = foundedYear;
+		this.officeAddress = officeAddress;
+		this.zipCode = zipCode;
+		this.siteAddress = siteAddress;
+		this.owner = owner;
+		this.ownerAgency = ownerAgency;
+		this.ceo = ceo;
+		this.generalManager = generalManager;
+		this.director = director;
+		this.logoUrl = logoUrl;
+	}
+
+	public static BaseballTeamEntity create(
+		TeamCode teamCode,
+		String teamName,
+		String teamNameEn,
+		String sponsor,
+		String homeGround,
+		Integer foundedYear,
+		String officeAddress,
+		String zipCode,
+		String siteAddress,
+		String owner,
+		String generalManager,
+		String director,
+		String ownerAgency,
+		String ceo,
+		String logoUrl
+	) {
+		return new BaseballTeamEntity(
+			teamCode, teamName, teamNameEn, sponsor, homeGround, foundedYear,
+			officeAddress, zipCode, siteAddress, owner,
+			ownerAgency, ceo, generalManager, director, logoUrl
+		);
+	}
+}
