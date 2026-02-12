d5615fe00ae36b87009a0071281aa60bbd6bfad4
suyeon89 <whtndus0809@naver.com>
2026-02-12 18:03:14 +0900

    feat:
    baseball_winner_history Entity 생성

---
+++ src/main/java/com/goti/domain/entity/baseball/BaseballWinnerHistoryEntity.java
@@ -0,0 +1,40 @@
+package com.goti.domain.entity.baseball;
+
+import com.goti.domain.base.ModificationTimestampEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.*;
+
+@Getter
+@Entity
+@Table(
+	name = "baseball_winner_history",
+	uniqueConstraints = {
+		@UniqueConstraint(name = "uk_winner_history_team_year", columnNames = {"baseball_team_id", "winning_year"})
+	})
+@NoArgsConstructor(access = PROTECTED)
+public class BaseballWinnerHistoryEntity extends ModificationTimestampEntity {
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "baseball_team_id", nullable = false)
+	private BaseballTeamEntity baseballTeam;
+
+	@Column(nullable = false)
+	private Integer winningYear;
+
+	private BaseballWinnerHistoryEntity(BaseballTeamEntity baseballTeam, Integer winningYear) {
+		this.baseballTeam = baseballTeam;
+		this.winningYear = winningYear;
+	}
+	public static BaseballWinnerHistoryEntity create(BaseballTeamEntity baseballTeam, Integer winningYear) {
+		return new BaseballWinnerHistoryEntity(baseballTeam, winningYear);
+	}
+}
