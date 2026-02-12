51f6f252119ed99580549ee714cc6d5ed95ecfa5
AkiStory <sonsungjeon@gmail.com>
2026-02-12 15:22:36 +0900

    feat: #7 Member Entity 작성

---
+++ src/main/java/com/goti/domain/entity/user/MemberEntity.java
@@ -0,0 +1,83 @@
+package com.goti.domain.entity.user;
+
+import static lombok.AccessLevel.*;
+
+import java.time.LocalDate;
+
+import org.springframework.util.StringUtils;
+
+import com.goti.common.validation.Preconditions;
+import com.goti.constants.Gender;
+import com.goti.constants.UserRole;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "member")
+@DiscriminatorValue("MEMBER")
+@NoArgsConstructor(access = PROTECTED)
+public class MemberEntity extends UserEntity {
+	private MemberEntity(
+		String email,
+		String mobile,
+		String name,
+		Gender gender,
+		LocalDate birthDate
+	) {
+		super(
+			email,
+			mobile,
+			name,
+			gender,
+			birthDate,
+			UserRole.MEMBER
+		);
+	}
+
+	public static MemberEntity create(
+		final String email,
+		final String mobile,
+		final String name,
+		final Gender gender,
+		final LocalDate birthDate
+	) {
+		validate(email, mobile, name, gender, birthDate);
+		return new MemberEntity(email, mobile, name, gender, birthDate);
+	}
+
+	private static void validate(
+		String email,
+		String mobile,
+		String name,
+		Gender gender,
+		LocalDate birthDate
+	) {
+		Preconditions.domainValidate(
+			StringUtils.hasText(email), "회원 이메일은 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			StringUtils.hasText(mobile), "회원 휴대전화 번호는 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			StringUtils.hasText(name), "회원 이름은 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			gender != null, "회원 성별은 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			birthDate != null, "회원 생년월일은 비어 있을 수 없습니다."
+		);
+
+		Preconditions.domainValidate(
+			birthDate.isBefore(LocalDate.now()), "회원 생년월일은 과거 날짜여야 합니다."
+		);
+	}
+}
+
