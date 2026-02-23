aa964fadbe70612f0b7007f6360dfc14b527aaca
junsang-ee <dkwnstkddk@gmail.com>
2026-02-23 17:38:22 +0900

    feat: #29 AuthApplicationService (login method) 추가 accessToken 요청 및 발급 api 로직 구현 (외부 api -> spring server) (getAccessToken) state 생성 및 실패 케이스 테스트 (AuthApplicationServiceTest)

---
+++ src/main/java/com/goti/service/auth/application/AuthApplicationService.java
@@ -0,0 +1,69 @@
+package com.goti.service.auth.application;
+
+import com.goti.constants.ProviderType;
+import com.goti.constants.messages.ErrorCode;
+import com.goti.constants.redis.RedisKey;
+import com.goti.exception.CustomException;
+import com.goti.infra.api.client.SocialApiClient;
+import com.goti.infra.api.client.SocialClientProvider;
+
+import com.goti.infra.api.dto.response.common.SocialStateResponse;
+import com.goti.infra.cache.RedisCache;
+
+import lombok.RequiredArgsConstructor;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AuthApplicationService {
+	private final SocialClientProvider socialClientProvider;
+	private final RedisCache redisCache;
+
+	static final String KEY_SEPARATOR = ":";
+
+	public void login(ProviderType provider, String code, String state) {
+		validateState(provider, state);
+		String accessToken = getAccessToken(provider, code, state);
+		log.info("accessToken :: {}", accessToken);
+	}
+
+
+	public SocialStateResponse issueState(ProviderType provider) {
+		if (provider == ProviderType.KAKAO) {
+			throw new CustomException(ErrorCode.BAD_REQUEST);
+		}
+		String state = UUID.randomUUID().toString();
+		String keyParam = provider + KEY_SEPARATOR + state;
+
+		redisCache.set(RedisKey.OAUTH_STATE, keyParam, true);
+
+		return SocialStateResponse.of(state);
+	}
+
+	private void validateState(ProviderType provider, String state) {
+		if (provider == ProviderType.KAKAO) return;
+
+		if (state == null || state.isBlank()) {
+			throw new CustomException(ErrorCode.MISSING_PARAMETER, "state");
+		}
+		String keyParam = provider + KEY_SEPARATOR + state;
+		String stateKey = RedisKey.OAUTH_STATE.getKey(keyParam);
+		if (!redisCache.consume(stateKey)) {
+			throw new CustomException(ErrorCode.INVALID_STATE);
+		}
+	}
+
+	private String getAccessToken(ProviderType provider, String code, String state) {
+		SocialApiClient apiClient = socialClientProvider.getClient(provider);
+		return apiClient.getAccessToken(code, state);
+	}
+
+	// todo: proverId 발급 로직 구현 예정
+
+}
---
+++ src/test/java/com/goti/service/auth/application/AuthApplicationServiceTest.java
@@ -0,0 +1,85 @@
+package com.goti.service.auth.application;
+
+import com.goti.constants.ProviderType;
+
+import com.goti.constants.messages.ErrorCode;
+import com.goti.constants.redis.RedisKey;
+import com.goti.exception.CustomException;
+
+import com.goti.infra.api.dto.response.common.SocialStateResponse;
+
+import com.goti.infra.cache.RedisCache;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@Slf4j
+@ActiveProfiles("test")
+@SpringBootTest
+public class AuthApplicationServiceTest {
+
+	@Autowired
+	AuthApplicationService authApplicationService;
+
+	@Autowired
+	RedisCache redisCache;
+
+	static final String KEY_SEPARATOR = ":";
+
+	@Test
+	@DisplayName("method : issueState()")
+	void state_생성_성공() {
+		ProviderType provider = ProviderType.NAVER;
+		SocialStateResponse response = authApplicationService.issueState(provider);
+		assertNotNull(response);
+		String state = response.state();
+		log.info("state :: {}", response.state());
+		String keyParam = provider + KEY_SEPARATOR + state;
+		boolean isExists = redisCache.get(RedisKey.OAUTH_STATE.getKey(keyParam), Boolean.class);
+		assertTrue(isExists);
+		log.info("isExists :: {}", isExists);
+	}
+
+	@Test
+	@DisplayName("method : issueState()")
+	void state_생성_실패_kakao() {
+		ProviderType provider = ProviderType.KAKAO;
+		assertThatThrownBy(
+			() -> authApplicationService.issueState(provider)
+		).isInstanceOf(CustomException.class)
+			.hasMessageContaining(ErrorCode.BAD_REQUEST.getMessage());
+	}
+
+	@ParameterizedTest
+	@NullAndEmptySource
+	@DisplayName("method : login() (google, naver)")
+	void 엑세스토큰_생성_실패_state_필수_소셜로그_state_null_또는_공백(String code) {
+		ProviderType provider = ProviderType.NAVER; // (or GOOGLE)
+		assertThatThrownBy(
+			() -> authApplicationService.login(provider, code, null)
+		).isInstanceOf(CustomException.class)
+			.hasMessageContaining(ErrorCode.MISSING_PARAMETER.format("state"));
+	}
+
+	@Test
+	@DisplayName("method : login() (google, naver)")
+	void 엑세스토큰_생성_실패_state_필수_소셜로그_redis_state_미존재() {
+		ProviderType provider = ProviderType.NAVER; // (or GOOGLE)
+		String code = "testCode";
+		String state = "NOT_EXISTS_STATE";
+		assertThatThrownBy(
+			() -> authApplicationService.login(provider, code, state)
+		).isInstanceOf(CustomException.class)
+			.hasMessageContaining(ErrorCode.INVALID_STATE.getMessage());
+	}
+}
