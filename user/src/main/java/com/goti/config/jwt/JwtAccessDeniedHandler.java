6c4d76a9c567a1766a3901972f15a2943cd45051
AkiStory <sonsungjeon@gmail.com>
2026-02-24 14:28:54 +0900

    refactor: #32 user 도메인

--- user/src/main/java/com/goti/config/jwt/JwtAccessDeniedHandler.java
+++ user/src/main/java/com/goti/config/jwt/JwtAccessDeniedHandler.java
@@ -1,7 +1,5 @@
 package com.goti.config.jwt;
 
-import com.goti.constants.messages.ErrorCode;
-import com.goti.exception.CustomException;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
@@ -11,6 +9,9 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerExceptionResolver;
+
+import com.goti.constants.messages.ErrorCode;
+import com.goti.exception.CustomException;
 
 @Component
 public class JwtAccessDeniedHandler implements AccessDeniedHandler {
