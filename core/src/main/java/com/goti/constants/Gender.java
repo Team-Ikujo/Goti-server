47a53d476e7738270f96f60f1977f041b40b5a92
junsang-ee <dkwnstkddk@gmail.com>
2026-02-26 17:38:24 +0900

    feat: #31 Gender fromString() convertMethod 추가

--- core/src/main/java/com/goti/constants/Gender.java
+++ core/src/main/java/com/goti/constants/Gender.java
@@ -1,11 +1,27 @@
 package com.goti.constants;
 
+import com.goti.constants.messages.ErrorCode;
+import com.goti.exception.CustomException;
+
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
 public enum Gender {
 	MALE("남"),
-	FEMALE("여");
+	FEMALE("여"),
+	UNKNOWN("알수없음")
+	;
 
 	private final String description;
+
+	public static Gender fromString(String value) {
+		if (value == null || value.isBlank())
+			return UNKNOWN;
+
+		return switch (value.toLowerCase()) {
+			case "m", "male" -> MALE;
+			case "f", "female" -> FEMALE;
+			default -> throw new CustomException(ErrorCode.INVALID_FORMAT, "성별");
+		};
+	}
 }
