f77390142d785f78aba5616f6612ae6db7696ec9
suyeon89 <whtndus0809@naver.com>
2026-03-03 18:00:46 +0900

    feat:
    feat: JPA Auditing 설정 추가

--- api/src/main/java/com/goti/GotiApplication.java
+++ api/src/main/java/com/goti/GotiApplication.java
@@ -3,9 +3,11 @@ package com.goti;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
+@EnableJpaAuditing
 public class GotiApplication {
 
 	public static void main(String[] args) {
