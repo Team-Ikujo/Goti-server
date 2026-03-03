cb55a03130a01b6887713c3202979edc76ef6ec1
suyeon89 <whtndus0809@naver.com>
2026-03-03 17:12:19 +0900

    feat:
    야구 경기 생성 애플리케이션 서비스 구현

---
+++ ticketing/src/main/java/com/goti/game/service/BaseballGameApplicationService.java
@@ -0,0 +1,53 @@
+package com.goti.game.service;
+
+import com.goti.constants.messages.ErrorCode;
+import com.goti.domain.entity.game.BaseballGameEntity;
+import com.goti.domain.entity.game.BaseballGameStatusEntity;
+import com.goti.game.dto.response.GameResponse;
+import com.goti.game.repository.BaseballGameRepository;
+import com.goti.game.repository.BaseballGameStatusRepository;
+
+import com.goti.game.service.command.CreateGameCommand;
+
+import com.goti.global.validation.Preconditions;
+
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class BaseballGameApplicationService {
+	private final BaseballGameRepository baseballGameRepository;
+	private final BaseballGameStatusRepository baseballGameStatusRepository;
+
+	@Transactional
+	public GameResponse create(CreateGameCommand cmd) {
+		boolean exists = baseballGameRepository.existsByHomeTeamIdAndAwayTeamIdAndPlayDateAndStartAt(
+			cmd.homeTeamId(),
+			cmd.awayTeamId(),
+			cmd.playDate(),
+			cmd.startAt()
+		);
+
+		Preconditions.validate(!exists, ErrorCode.GAME_ALREADY_EXISTS);
+
+		BaseballGameEntity game = BaseballGameEntity.create(
+			cmd.homeTeamId(),
+			cmd.awayTeamId(),
+			cmd.stadiumId(),
+			cmd.playDate(),
+			cmd.startAt(),
+			cmd.leagueType(),
+			cmd.reservationOpenedAt(),
+			cmd.reservationClosedAt()
+		);
+		BaseballGameEntity savedGame = baseballGameRepository.save(game);
+
+		BaseballGameStatusEntity gameStatusEntity = BaseballGameStatusEntity.init(savedGame);
+		baseballGameStatusRepository.save(gameStatusEntity);
+
+		return GameResponse.from(savedGame);
+	}
+}
