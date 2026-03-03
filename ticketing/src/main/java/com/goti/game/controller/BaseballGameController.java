6a2074a567ee951a811f6cd0cbd3ecc595dd5cb0
suyeon89 <whtndus0809@naver.com>
2026-03-03 22:47:40 +0900

    refactor:
    static import로 변경

--- ticketing/src/main/java/com/goti/game/controller/BaseballGameController.java
+++ ticketing/src/main/java/com/goti/game/controller/BaseballGameController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import static com.goti.global.api.ApiSuccessResponse.*;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/games/baseball")
@@ -26,6 +28,6 @@ public class BaseballGameController {
 		@Valid @RequestBody CreateGameRequest request
 	) {
 		GameResponse response = baseballGameApplicationService.create(request.toCommand());
-		return ApiSuccessResponse.wrap(response);
+		return wrap(response);
 	}
 }
