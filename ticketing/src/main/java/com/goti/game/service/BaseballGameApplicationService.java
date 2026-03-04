b856e8a4bfba445f92c9c78609793a073ba59d82
suyeon89 <whtndus0809@naver.com>
2026-03-04 23:46:27 +0900

    feat:
    경기 전체 조회 서비스 로직 구현

--- ticketing/src/main/java/com/goti/game/service/BaseballGameApplicationService.java
+++ ticketing/src/main/java/com/goti/game/service/BaseballGameApplicationService.java
@@ -1,20 +1,20 @@
 package com.goti.game.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import com.goti.constants.messages.ErrorCode;
 import com.goti.domain.entity.game.BaseballGameEntity;
 import com.goti.domain.entity.game.BaseballGameStatusEntity;
 import com.goti.game.dto.response.GameResponse;
 import com.goti.game.repository.BaseballGameRepository;
 import com.goti.game.repository.BaseballGameStatusRepository;
-
 import com.goti.game.service.command.CreateGameCommand;
-
 import com.goti.global.validation.Preconditions;
 
 import lombok.RequiredArgsConstructor;
-
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -48,5 +48,11 @@ public class BaseballGameApplicationService {
 		baseballGameStatusRepository.save(gameStatusEntity);
 
 		return GameResponse.from(savedGame);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<GameResponse> getGames(Pageable pageable) {
+		return baseballGameRepository.findAll(pageable)
+			.map(GameResponse::from);
 	}
 }
