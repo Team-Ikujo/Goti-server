c0a353e8c152c11117df0a8ae82342f1872f4901
junsang-ee <dkwnstkddk@gmail.com>
2026-03-05 15:42:05 +0900

    feat: #73 Email annotation 추가 (signupRequest email 검증)

--- user/src/main/java/com/goti/dto/request/SignupRequest.java
+++ user/src/main/java/com/goti/dto/request/SignupRequest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.goti.config.validator.ValidMobile;
 import com.goti.constants.Gender;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Past;
@@ -15,6 +16,7 @@ public record SignupRequest(
 	@NotBlank(message = "소셜 검증 토큰은 필수 항목입니다.")
 	String socialVerifyToken,
 
+	@Email
 	@NotBlank(message = "이메일은 필수 항목입니다.")
 	String email,
 
