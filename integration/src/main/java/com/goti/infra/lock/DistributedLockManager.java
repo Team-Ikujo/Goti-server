bf387e1866a91b2157dbf6ce9ac9998e34eaefb4
suyeon89 <whtndus0809@naver.com>
2026-03-05 23:28:02 +0900

    feat:
    좌석 점유용 DistributedLockManager 추가

---
+++ integration/src/main/java/com/goti/infra/lock/DistributedLockManager.java
@@ -0,0 +1,42 @@
+package com.goti.infra.lock;
+
+import com.goti.constants.messages.ErrorCode;
+import com.goti.exception.CustomException;
+import com.goti.global.validation.Preconditions;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+@Component
+@RequiredArgsConstructor
+public class DistributedLockManager {
+	private static final long WAIT_TIME_SECONDS = 1L;
+	private static final long LEASE_TIME_SECONDS = 3L;
+
+	private final RedissonClient redissonClient;
+
+	public <T> T withLock(String lockKey, Supplier<T> action) {
+		RLock lock = redissonClient.getLock(lockKey);
+		boolean acquired = false;
+
+		try {
+			acquired = lock.tryLock(WAIT_TIME_SECONDS, LEASE_TIME_SECONDS, TimeUnit.SECONDS);
+			Preconditions.validate(
+				acquired,
+				ErrorCode.SEAT_LOCK_ACQUIRE_FAILED
+			);
+			return action.get();
+		} catch (InterruptedException e) {
+			Thread.currentThread().interrupt();
+			throw new CustomException(ErrorCode.INTERNAL_SERVER_ERROR, e);
+		} finally {
+			if (acquired && lock.isHeldByCurrentThread()) {
+				lock.unlock();
+			}
+		}
+	}
+}
