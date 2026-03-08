3d9133f782faaab1b10250b6ea1f5524e4cff80c
suyeon89 <whtndus0809@naver.com>
2026-03-09 04:36:52 +0900

    feat:
    좌석 상태 조회 서비스 로직 작성

--- ticketing/src/main/java/com/goti/seat/repository/SeatStatusRepository.java
+++ ticketing/src/main/java/com/goti/seat/repository/SeatStatusRepository.java
@@ -1,13 +1,16 @@
 package com.goti.seat.repository;
 
-import com.goti.domain.entity.seat.SeatStatusEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.goti.domain.entity.seat.SeatStatusEntity;
+
 @Repository
 public interface SeatStatusRepository extends JpaRepository<SeatStatusEntity, UUID> {
+	List<SeatStatusEntity> findAllByGame_IdAndSeat_SeatSection_Id(UUID gameId, UUID sectionId);
 	Optional<SeatStatusEntity> findByGame_IdAndSeat_Id(UUID gameId, UUID seatId);
 }
---
+++ ticketing/src/main/java/com/goti/seat/service/domain/SeatStatusService.java
@@ -0,0 +1,10 @@
+package com.goti.seat.service.domain;
+
+import java.util.List;
+import java.util.UUID;
+
+import com.goti.seat.dto.response.GameSeatStatusResponse;
+
+public interface SeatStatusService {
+	List<GameSeatStatusResponse> get(UUID gameId, UUID sectionId);
+}
