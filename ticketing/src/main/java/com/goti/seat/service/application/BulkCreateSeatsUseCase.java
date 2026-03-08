39b09273921474d396ccbfd0955addad8af023de
suyeon89 <whtndus0809@naver.com>
2026-03-08 23:52:05 +0900

    feat:
    좌석 일괄 생성 API 구현

--- ticketing/src/main/java/com/goti/seat/service/application/BulkCreateSeatsUseCase.java
+++ ticketing/src/main/java/com/goti/seat/service/application/BulkCreateSeatsUseCase.java
@@ -1,11 +1,9 @@
 package com.goti.seat.service.application;
 
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
-import com.goti.domain.entity.seat.SeatEntity;
 import com.goti.seat.dto.response.BulkCreateSeatsResponse;
 import com.goti.seat.service.domain.BulkCreateSeatsService;
 
@@ -22,7 +20,7 @@ public class BulkCreateSeatsUseCase {
 		Integer startSeatNumber,
 		Integer endSeatNumber
 	) {
-		List<SeatEntity> seats = bulkCreateSeatsService.create(
+		bulkCreateSeatsService.create(
 			sectionId,
 			rowName,
 			startSeatNumber,
