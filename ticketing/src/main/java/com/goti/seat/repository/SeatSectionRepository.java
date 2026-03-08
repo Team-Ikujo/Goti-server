1afa143a5f5572d19710f9b06a823bebb92aad9a
suyeon89 <whtndus0809@naver.com>
2026-03-08 18:02:34 +0900

    refactor:
    좌석 구역 조회 시 seatGrade 연관 엔티티 즉시 조회 적용

--- ticketing/src/main/java/com/goti/seat/repository/SeatSectionRepository.java
+++ ticketing/src/main/java/com/goti/seat/repository/SeatSectionRepository.java
@@ -3,6 +3,7 @@ package com.goti.seat.repository;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +13,6 @@ import com.goti.domain.entity.seat.SeatSectionEntity;
 public interface SeatSectionRepository extends JpaRepository<SeatSectionEntity, UUID> {
 	boolean existsByStadiumIdAndSectionCode(UUID stadiumId, String sectionCode);
 
+	@EntityGraph(attributePaths = "seatGrade")
 	List<SeatSectionEntity> findAllByStadiumId(UUID stadiumId);
 }
