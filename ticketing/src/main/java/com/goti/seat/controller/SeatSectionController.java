ce5f45165d731b6cfed8ab0c43bc776dcb23ca97
suyeon89 <whtndus0809@naver.com>
2026-03-08 16:32:48 +0900

    feat:
    좌석 구역 생성 API 구현

---
+++ ticketing/src/main/java/com/goti/seat/controller/SeatSectionController.java
@@ -0,0 +1,44 @@
+package com.goti.seat.controller;
+
+import static com.goti.global.api.ApiSuccessResponse.wrap;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.goti.global.api.ApiSuccessResponse;
+import com.goti.seat.dto.request.CreateSeatSectionRequest;
+import com.goti.seat.dto.response.SeatSectionResponse;
+import com.goti.seat.service.application.SeatSectionApplicationService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+
+@Tag(name = "Seat Section", description = "좌석 구역 생성 API")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/seats/sections")
+public class SeatSectionController {
+	private final SeatSectionApplicationService seatSectionApplicationService;
+
+	@Operation(
+		summary = "좌석 구역 생성",
+		description = "좌석 등급에 속한 좌석 구역을 생성하는 API"
+	)
+	@PostMapping
+	public ResponseEntity<ApiSuccessResponse<SeatSectionResponse>> create(
+		@Valid @RequestBody CreateSeatSectionRequest request
+	) {
+		SeatSectionResponse response = seatSectionApplicationService.create(
+			request.gradeId(),
+			request.stadiumId(),
+			request.sectionCode(),
+			request.capacity()
+		);
+		return wrap(response);
+	}
+}
