d9e524b120c2b4396ec6be29750d8180e5a542a7
suyeon89 <whtndus0809@naver.com>
2026-03-08 17:57:16 +0900

    refactor:
    좌석 등급 생성 및 조회 로직 도메인 서비스로 분리

--- ticketing/src/main/java/com/goti/seat/service/application/SeatGradeService.java
+++ ticketing/src/main/java/com/goti/seat/service/application/SeatGradeService.java
@@ -24,14 +24,4 @@ public class SeatGradeService {
 	public List<SeatGradeResponse> get(UUID stadiumId) {
 		return seatGradeService.get(stadiumId);
 	}
-
-	@Transactional(readOnly = true)
-	public List<SeatGradeResponse> getSeatGrades(UUID stadiumId) {
-		// TODO: 유저 인증 여부 확인
-		// Preconditions.validate(userId != null, ErrorCode.AUTH_INVALID);
-
-		return seatGradeRepository.findAllByStadiumId(stadiumId).stream()
-			.map(SeatGradeResponse::from)
-			.toList();
-	}
 }
