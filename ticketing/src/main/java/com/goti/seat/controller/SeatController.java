878c25bc62ca7a0887f138cdfe5ab9e060387f3a
suyeon89 <whtndus0809@naver.com>
2026-03-09 03:59:32 +0900

    feat:
    구역 별 좌석 조회 API 작성

--- ticketing/src/main/java/com/goti/seat/controller/SeatController.java
+++ ticketing/src/main/java/com/goti/seat/controller/SeatController.java
@@ -2,15 +2,21 @@ package com.goti.seat.controller;
 
 import static com.goti.global.api.ApiSuccessResponse.*;
 
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goti.global.api.ApiSuccessResponse;
 import com.goti.seat.dto.request.BulkCreateSeatsRequest;
 import com.goti.seat.dto.response.BulkCreateSeatsResponse;
+import com.goti.seat.dto.response.SeatResponse;
 import com.goti.seat.service.domain.SeatService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -40,5 +46,16 @@ public class SeatController {
 			request.endSeatNumber()
 		);
 		return wrap(response);
+	}
+
+	@Operation(
+		summary = "좌석 조회",
+		description = "좌석 구역별 좌석 목록을 조회하는 API"
+	)
+	@GetMapping
+	public ResponseEntity<ApiSuccessResponse<List<SeatResponse>>> list(
+		@RequestParam UUID sectionId
+	) {
+		return wrap(seatService.get(sectionId));
 	}
 }
