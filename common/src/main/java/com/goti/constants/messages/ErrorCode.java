71c2c39e370f77953068602f7b177df2208431e3
suyeon89 <whtndus0809@naver.com>
2026-03-10 02:02:54 +0900

    feat:
    점유 좌석 기반 주문 생성 및 가격 계산 로직 구현

--- common/src/main/java/com/goti/constants/messages/ErrorCode.java
+++ common/src/main/java/com/goti/constants/messages/ErrorCode.java
@@ -69,6 +69,8 @@ public enum ErrorCode {
 	SEAT_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 생성된 좌석이 포함되어 있습니다."),
 	SEAT_SECTION_CAPACITY_EXCEEDED(HttpStatus.BAD_REQUEST, "좌석 구역의 수용 인원을 초과할 수 없습니다."),
 	TICKET_PRICE_CONDITION_ALREADY_EXISTS(HttpStatus.CONFLICT, "이미 동일한 티켓 가격 조건이 존재합니다."),
+	TICKET_PRICING_POLICY_NOT_FOUND(HttpStatus.NOT_FOUND, "적용 가능한 가격 정책을 찾을 수 없습니다."),
+	TICKET_PRICE_NOT_FOUND(HttpStatus.NOT_FOUND, "적용 가능한 티켓 가격을 찾을 수 없습니다."),
 	DUPLICATE_HOLD_ID_REQUEST(HttpStatus.BAD_REQUEST, "같은 좌석이 중복 선택되었습니다."),
 
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 회원입니다."),
