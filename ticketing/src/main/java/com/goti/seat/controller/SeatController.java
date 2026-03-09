75a75c8542dbf5d9c44e41ab03a79e6da027553e
suyeon89 <whtndus0809@naver.com>
2026-03-09 12:29:35 +0900

    refactor:
    swagger description 수정

--- ticketing/src/main/java/com/goti/seat/controller/SeatController.java
+++ ticketing/src/main/java/com/goti/seat/controller/SeatController.java
@@ -33,7 +33,7 @@ public class SeatController {
 
 	@Operation(
 		summary = "좌석 일괄 생성",
-		description = "좌석 구역의 특정 행에 대한 좌석 번호 범위 일괄 생성 API"
+		description = "좌석 번호 범위 일괄 생성 API"
 	)
 	@PostMapping("/bulk")
 	public ResponseEntity<ApiSuccessResponse<BulkCreateSeatsResponse>> createBulk(
