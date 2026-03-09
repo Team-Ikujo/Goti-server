a7b29394cb114674348cdaf1b306bd11430621c3
junsang-ee <dkwnstkddk@gmail.com>
2026-03-09 16:25:54 +0900

    refactor: #131 TeamCode(Enum) description 추가

--- core/src/main/java/com/goti/constants/TeamCode.java
+++ core/src/main/java/com/goti/constants/TeamCode.java
@@ -1,14 +1,22 @@
 package com.goti.constants;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum TeamCode {
-	LG,
-	DO,
-	KIW,
-	HH,
-	KT,
-	SSG,
-	KIA,
-	LOT,
-	NC,
-	SS
+	LG("LG Twins"),
+	DO("Doosan Bears"),
+	KIW("Kiwoom Heroes"),
+	HH("Hanwha Eagles"),
+	KT("KT Wiz"),
+	SSG("SSG Landers"),
+	KIA("KIA Tigers"),
+	LOT("Lotte Giants"),
+	NC("NC Dinos"),
+	SS("Samsung Lions")
+	;
+
+	private final String description;
 }
