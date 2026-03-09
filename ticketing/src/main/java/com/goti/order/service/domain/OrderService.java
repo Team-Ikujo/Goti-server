5550e01f424c1fa8d8b1c3133cf08f035b20f4ae
suyeon89 <whtndus0809@naver.com>
2026-03-10 00:58:39 +0900

    refactor:
    도메인 서비스에서 구현

--- ticketing/src/main/java/com/goti/order/service/domain/OrderService.java
+++ ticketing/src/main/java/com/goti/order/service/domain/OrderService.java
@@ -2,16 +2,12 @@ package com.goti.order.service.domain;
 
 import java.util.UUID;
 
-import com.goti.domain.entity.game.GameScheduleEntity;
 import com.goti.order.dto.request.CreateOrderRequest;
 import com.goti.order.dto.response.CreateOrderResponse;
 
 public interface OrderService {
 	CreateOrderResponse create(
-		GameScheduleEntity gameSchedule,
 		UUID userId,
-		CreateOrderRequest request,
-		Integer totalQuantity,
-		Integer totalAmount
+		CreateOrderRequest request
 	);
 }
