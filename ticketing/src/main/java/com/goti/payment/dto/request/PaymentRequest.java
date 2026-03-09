e2d39f2c830f2162f357989562025ab124559ef0
suyeon89 <whtndus0809@naver.com>
2026-03-10 06:17:30 +0900

    feat:
    결제 요청/응답 dto 생성

---
+++ ticketing/src/main/java/com/goti/payment/dto/request/PaymentRequest.java
@@ -0,0 +1,20 @@
+package com.goti.payment.dto.request;
+
+import com.goti.constants.PaymentMethod;
+import com.goti.constants.PaymentStatus;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+@Schema(description = "mock 결제 요청")
+public record PaymentRequest(
+	@Schema(description = "결제 수단", example = "CARD")
+	@NotNull(message = "결제 수단은 필수입니다.")
+	PaymentMethod paymentMethod,
+
+	@Schema(description = "결제 멱등 키", example = "payment-idempotency-key")
+	@NotBlank(message = "결제 멱등 키는 필수입니다.")
+	String idempotencyKey
+) {
+}
