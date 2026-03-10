0ccd4e25d6fd4f6b2c99e3f284f50244b2b1ffc6
suyeon89 <whtndus0809@naver.com>
2026-03-10 12:23:28 +0900

    refactor: #161
    좌석 상태 조회 api 유저 인증 추가

--- ticketing/src/main/java/com/goti/seat/service/domain/SeatStatusService.java
+++ ticketing/src/main/java/com/goti/seat/service/domain/SeatStatusService.java
@@ -6,5 +6,5 @@ import java.util.UUID;
 import com.goti.seat.dto.response.GameSeatStatusResponse;
 
 public interface SeatStatusService {
-	List<GameSeatStatusResponse> get(UUID gameId, UUID sectionId);
+	List<GameSeatStatusResponse> get(UUID gameId, UUID sectionId, UUID userId);
 }
