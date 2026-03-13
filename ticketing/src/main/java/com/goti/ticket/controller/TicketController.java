41d71c35ffbe659705b23d3cd7c3cb75f92b685a
suyeon89 <whtndus0809@naver.com>
2026-03-13 18:40:19 +0900

    refactor: #168
    티켓 qr 조회 api 제거 (조회 브랜치에서 작업)

--- ticketing/src/main/java/com/goti/ticket/controller/TicketController.java
+++ ticketing/src/main/java/com/goti/ticket/controller/TicketController.java
@@ -1,21 +1,8 @@
 package com.goti.ticket.controller;
 
-import static com.goti.global.api.ApiSuccessResponse.wrap;
-
-import java.util.UUID;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.goti.global.api.ApiSuccessResponse;
-import com.goti.ticket.dto.response.TicketQrResponse;
-import com.goti.ticket.service.application.TicketQrService;
-
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
@@ -24,17 +11,4 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/tickets")
 public class TicketController {
-	private final TicketQrService ticketQrService;
-
-	@Operation(
-		summary = "모바일 티켓 QR 발급",
-		description = "모바일 티켓 QR 토큰을 발급하는 API"
-	)
-	@GetMapping("/{ticketId}/qr")
-	public ResponseEntity<ApiSuccessResponse<TicketQrResponse>> QrCreate(
-		@PathVariable UUID ticketId,
-		@AuthenticationPrincipal(expression = "id") UUID userId
-	) {
-		return wrap(ticketQrService.create(ticketId, userId));
-	}
 }
