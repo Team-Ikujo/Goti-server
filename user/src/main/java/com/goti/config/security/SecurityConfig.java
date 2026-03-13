4699dda394677178ffd93ecc053a94f0c4a1a37f
AkiStory <sonsungjeon@gmail.com>
2026-03-13 18:59:04 +0900

    feat : #184 리셀 그래프를 위한 조회 api

--- user/src/main/java/com/goti/config/security/SecurityConfig.java
+++ user/src/main/java/com/goti/config/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 		"/api/v1/auth/**",
 		"/api/v1/stadiums/**",
 		"/api/v1/games/**",
+		"api/v1/resale/histories/**",
 		"/actuator/**",
 		"/swagger-ui/**",
 		"/v3/api-docs/**"
@@ -40,7 +41,6 @@ public class SecurityConfig {
 
 	public static final String[] PERMIT_MEMBER_PATH = {
 		"/api/v1/resale/listings/**",
-		"/api/v1/resale/transactions/**",
 		"/api/v1/resale/holds/**",
 		"/api/v1/resale/transactions/**",
 		"/api/v1/orders/**"
