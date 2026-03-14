de2a275295b69dd32becc999e594037b599a5102
suyeon89 <whtndus0809@naver.com>
2026-03-15 01:40:44 +0900

    refactor: #168
    사용안하는 메서드 제거

--- ticketing/src/main/java/com/goti/order/repository/OrderItemRepository.java
+++ ticketing/src/main/java/com/goti/order/repository/OrderItemRepository.java
@@ -1,7 +1,6 @@
 package com.goti.order.repository;
 
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -12,7 +11,6 @@ import com.goti.domain.entity.order.OrderItemEntity;
 
 @Repository
 public interface OrderItemRepository extends JpaRepository<OrderItemEntity, UUID>, OrderItemRepositoryCustom {
-	List<OrderItemEntity> findAllByOrder_Id(UUID orderId);
 	boolean existsOrderedSeats(
 		UUID gameId,
 		Collection<UUID> seatIds,
