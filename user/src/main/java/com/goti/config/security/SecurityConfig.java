29d0bb258253e9fb4a6a200f84d09beba3befcce
junsang-ee <dkwnstkddk@gmail.com>
2026-03-14 17:42:29 +0900

    refactor: #163 야구구단 생성 API endpoint -> SecurityConfig path 추가 (/api/v1/baseball-teams/**)

--- user/src/main/java/com/goti/config/security/SecurityConfig.java
+++ user/src/main/java/com/goti/config/security/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
 		"/api/v1/auth/**",
 		"/api/v1/stadiums/**",
 		"/api/v1/games/**",
+		"/api/v1/baseball-teams/**",
 		"/actuator/**",
 		"/swagger-ui/**",
 		"/v3/api-docs/**"
