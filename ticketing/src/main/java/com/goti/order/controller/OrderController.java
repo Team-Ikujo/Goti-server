3682b2e40f8969b8777e85bf9af8530e87a75f03
suyeon89 <whtndus0809@naver.com>
2026-03-15 02:27:50 +0900

    feat: #189
    Payment 모듈에서 호출하기 위한 결제 완료 후 티켓 발급 API 생성

--- ticketing/src/main/java/com/goti/order/controller/OrderController.java
+++ ticketing/src/main/java/com/goti/order/controller/OrderController.java
@@ -8,6 +8,7 @@ import java.util.UUID;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.goti.global.api.ApiSuccessResponse;
 import com.goti.order.dto.request.OrderCreateRequest;
+import com.goti.order.dto.request.OrderPaymentConfirmRequest;
 import com.goti.order.dto.response.OrderCreateResponse;
 import com.goti.order.dto.response.OrderListResponse;
+import com.goti.order.dto.response.OrderPaymentConfirmResponse;
 import com.goti.order.service.application.OrderCreateService;
+import com.goti.order.service.application.OrderPaymentConfirmService;
 import com.goti.order.service.domain.OrderService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -32,6 +36,7 @@ import lombok.RequiredArgsConstructor;
 public class OrderController {
 	private final OrderCreateService orderCreateService;
 	private final OrderService orderService;
+	private final OrderPaymentConfirmService orderPaymentConfirmService;
 
 	@Operation(
 		summary = "주문 생성",
@@ -54,5 +59,18 @@ public class OrderController {
 		@AuthenticationPrincipal(expression = "id") UUID memberId
 	) {
 		return wrap(orderService.getMyOrders(memberId));
+	}
+
+	//TODO: 추후 내부 호출용 API Controller 분리
+	@Operation(
+		summary = "주문 결제 완료 처리 (내부용)",
+		description = "결제 완료 후 티켓 발급 API"
+	)
+	@PostMapping("/{orderId}/payment-confirmations")
+	public ResponseEntity<ApiSuccessResponse<OrderPaymentConfirmResponse>> confirmPayment(
+		@PathVariable UUID orderId,
+		@Valid @RequestBody OrderPaymentConfirmRequest request
+	) {
+		return wrap(orderPaymentConfirmService.confirm(orderId, request.userId()));
 	}
 }
