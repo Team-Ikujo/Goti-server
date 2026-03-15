5fd3a8ae76248b958c915772dc0d74da5bce7551
suyeon89 <whtndus0809@naver.com>
2026-03-15 18:07:47 +0900

    refactor: #177
    조회 API 경로 수정

--- ticketing/src/main/java/com/goti/order/controller/OrderController.java
+++ ticketing/src/main/java/com/goti/order/controller/OrderController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
