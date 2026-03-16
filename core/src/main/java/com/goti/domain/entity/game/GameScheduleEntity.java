d302ce78e9f654bbf906acae9d4f07333f9cb760
junsang-ee <dkwnstkddk@gmail.com>
2026-03-16 10:59:50 +0900

    refactor: #179
    게임일정 엔티티 참조형 (mappedBy) 외부 참조 객체 정의 (graph 설정) gameStatus, ticketingStatus

--- core/src/main/java/com/goti/domain/entity/game/GameScheduleEntity.java
+++ core/src/main/java/com/goti/domain/entity/game/GameScheduleEntity.java
@@ -9,13 +9,13 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.UUID;
 
 import static lombok.AccessLevel.*;
@@ -40,6 +40,12 @@ public class GameScheduleEntity extends ModificationTimestampEntity {
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false)
 	private LeagueType leagueType;
+
+	@OneToOne(mappedBy = "gameSchedule", fetch = FetchType.LAZY)
+	private GameStatusEntity gameStatus;
+
+	@OneToOne(mappedBy = "gameSchedule", fetch = FetchType.LAZY)
+	private GameTicketingStatusEntity ticketingStatus;
 
 	private GameScheduleEntity(
 		UUID homeTeamId,
