147c183c8e27e82563812977ce4f3d862dea4bb6
junsang-ee <dkwnstkddk@gmail.com>
2026-03-20 04:34:23 +0900

    feat: #236
    야구구단(팀) 상세 조회 API 추가 및 BaseballTeamController API 일괄 Operation 추가 (description...)

--- stadium/src/main/java/com/goti/stadium/controller/BaseballTeamController.java
+++ stadium/src/main/java/com/goti/stadium/controller/BaseballTeamController.java
@@ -4,7 +4,12 @@ import static com.goti.global.api.ApiSuccessResponse.*;
 
 import java.util.UUID;
 
+import com.goti.stadium.domain.entity.team.BaseballTeamEntity;
+
+import io.swagger.v3.oas.annotations.Operation;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -32,6 +37,10 @@ public class BaseballTeamController {
 	private final BaseballTeamService baseballTeamService;
 	private final HomeStadiumManagementService homeStadiumManagementService;
 
+	@Operation(
+		summary = "구단(팀) 생성",
+		description = "야구구단(팀) 생성 API"
+	)
 	@PostMapping
 	public ResponseEntity<ApiSuccessResponse<BaseballTeamCreateResponse>> register(
 		@RequestBody @Valid BaseballTeamCreateRequest request
@@ -41,6 +50,23 @@ public class BaseballTeamController {
 		);
 	}
 
+	@Operation(
+		summary = "구단(팀) 상세 조회",
+		description = "야구구단(팀) 상세 조회 API"
+	)
+	@GetMapping("/{teamId}")
+	public ResponseEntity<ApiSuccessResponse<BaseballTeamEntity>> get(
+		@PathVariable UUID teamId
+	) {
+		return wrap(
+			baseballTeamService.getById(teamId)
+		);
+	}
+
+	@Operation(
+		summary = "구단(팀) 홈구장 등록",
+		description = "야구구단(팀) 홈구장(제1구장, 제2구장) 등록 API"
+	)
 	@PostMapping("/{teamId}/home-stadiums")
 	public ResponseEntity<ApiSuccessResponse<HomeStadiumCreateResponse>> assignHomeStadium(
 		@PathVariable UUID teamId,
