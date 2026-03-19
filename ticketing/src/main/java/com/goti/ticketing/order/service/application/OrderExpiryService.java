6c7741f6da741c91e44e19ea9569057afa3a693c
suyeon89 <whtndus0809@naver.com>
2026-03-19 16:38:45 +0900

    feat: #238
    holdId 기반 조회

--- ticketing/src/main/java/com/goti/ticketing/order/service/application/OrderExpiryService.java
+++ ticketing/src/main/java/com/goti/ticketing/order/service/application/OrderExpiryService.java
@@ -8,9 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.goti.constants.messages.ErrorCode;
 import com.goti.exception.CustomException;
-import com.goti.ticketing.constants.SeatHoldStatus;
 import com.goti.ticketing.domain.entity.order.OrderEntity;
 import com.goti.ticketing.domain.entity.order.OrderItemEntity;
+import com.goti.ticketing.domain.entity.seat.SeatHoldEntity;
 import com.goti.ticketing.order.repository.OrderItemRepository;
 import com.goti.ticketing.order.repository.OrderRepository;
 import com.goti.ticketing.seat.repository.SeatHoldRepository;
@@ -35,17 +35,13 @@ public class OrderExpiryService {
 
 		List<OrderItemEntity> orderItems = orderItemRepository.findOrderItemsByOrderId(orderId);
 		for (OrderItemEntity orderItem : orderItems) {
-			seatHoldRepository.findLatestActiveHold(
-				order.getGameSchedule(),
-				orderItem.getSeat(),
-				order.getMemberId(),
-				SeatHoldStatus.HOLDING
-			).ifPresent(seatHold -> {
-				seatStatusRepository.findByGameAndSeat(order.getGameSchedule(), orderItem.getSeat())
-					.orElseThrow(() -> new CustomException(ErrorCode.SEAT_STATUS_NOT_FOUND))
-					.release();
-				seatHold.release();
-			});
+			SeatHoldEntity seatHold = seatHoldRepository.findById(orderItem.getHoldId())
+				.orElseThrow(() -> new CustomException(ErrorCode.SEAT_HOLD_NOT_FOUND));
+
+			seatStatusRepository.findByGameAndSeat(order.getGameSchedule(), orderItem.getSeat())
+				.orElseThrow(() -> new CustomException(ErrorCode.SEAT_STATUS_NOT_FOUND))
+				.release();
+			seatHold.release();
 
 			orderItem.expire();
 		}
