d7168c52e7d726da17d16cd78e384e282950e9a4
junsang-ee <dkwnstkddk@gmail.com>
2026-03-19 11:46:38 +0900

    feat: #234
    RedisKey -> refresh token redis key 추가

--- integration/src/main/java/com/goti/infra/constants/redis/RedisKey.java
+++ integration/src/main/java/com/goti/infra/constants/redis/RedisKey.java
@@ -10,7 +10,8 @@ import lombok.RequiredArgsConstructor;
 public enum RedisKey {
 	SMS_AUTH_CODE("auth:sms:", Duration.ofMinutes(3)),
 	OAUTH_STATE("oauth:state:", Duration.ofMinutes(5)),
-	TICKET_QR("ticket:qr-token:", Duration.ofMinutes(3));
+	TICKET_QR("ticket:qr-token:", Duration.ofMinutes(3)),
+	REFRESH_TOKEN("auth:refresh-token:", Duration.ofDays(7));
 
 	private final String prefix;
 
