191263229cb0802911854c85638461e2272a2592
junsang-ee <dkwnstkddk@gmail.com>
2026-03-20 04:09:29 +0900

    feat: #236
    ApiEndpointProperties 추가 및 application.yml(in api module) 정의

---
+++ integration/src/main/java/com/goti/config/properties/ApiEndpointProperties.java
@@ -0,0 +1,9 @@
+package com.goti.config.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "infra.api.endpoints")
+public record ApiEndpointProperties(
+	String stadium
+) {
+}
