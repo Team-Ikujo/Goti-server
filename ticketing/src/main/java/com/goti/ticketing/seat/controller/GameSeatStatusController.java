58a2e1c59ff0465758c136f21be3bb69fd010f92
suyeon89 <whtndus0809@naver.com>
2026-03-24 12:42:31 +0900

    feat: #291
    좌석 상태 초기 생성 API 생성

--- ticketing/src/main/java/com/goti/ticketing/seat/controller/GameSeatStatusController.java
+++ ticketing/src/main/java/com/goti/ticketing/seat/controller/GameSeatStatusController.java
@@ -9,11 +9,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.goti.global.api.ApiSuccessResponse;
+import com.goti.ticketing.seat.dto.response.GameSeatStatusInitResponse;
 import com.goti.ticketing.seat.dto.response.GameSeatStatusResponse;
+import com.goti.ticketing.seat.service.application.GameSeatStatusInitService;
 import com.goti.ticketing.seat.service.domain.SeatStatusService;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -26,6 +29,18 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/api/v1/game-seats")
 public class GameSeatStatusController {
 	private final SeatStatusService seatStatusService;
+	private final GameSeatStatusInitService gameSeatStatusInitService;
+
+	@Operation(
+		summary = "경기 좌석 상태 초기화",
+		description = "해당 경기 구장의 모든 좌석에 대해 좌석 상태 AVAILABLE로 생성"
+	)
+	@PostMapping("/{gameId}/init")
+	public ResponseEntity<ApiSuccessResponse<GameSeatStatusInitResponse>> init(
+		@PathVariable UUID gameId
+	) {
+		return wrap(gameSeatStatusInitService.init(gameId));
+	}
 
 	@Operation(
 		summary = "경기별 좌석 상태 조회",
