e8e8334da03e8980affda9ea192896c7922348de
AkiStory <sonsungjeon@gmail.com>
2026-03-27 23:44:03 +0900

    refactor : #305 enum 기획 화면과 일치시킴

--- resale/src/main/java/com/goti/resale/constants/ResaleListingOrderStatus.java
+++ resale/src/main/java/com/goti/resale/constants/ResaleListingOrderStatus.java
@@ -6,11 +6,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ResaleListingOrderStatus {
-	LISTING("판매등록"),
+	LISTING("판매중"),
 	PARTIAL("부분 처리"),
-	SOLD("판매완료(정산 대기)"),
-	SETTLED("판매완료(정산 완료)"),
-	CANCELED("판매취소");
+	SOLD("판매 완료(정산 대기)"),
+	SETTLED("판매 완료(정산 완료)"),
+	CANCELED("판매 취소");
 
 	private final String description;
 }
