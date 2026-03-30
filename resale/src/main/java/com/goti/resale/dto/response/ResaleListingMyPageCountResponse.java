69d72b13b69f6770ef11df7d8affe933bec25732
AkiStory <sonsungjeon@gmail.com>
2026-03-30 15:16:22 +0900

    feat : #305
    마이페이지 리셀 조회를 위한 dto

---
+++ resale/src/main/java/com/goti/resale/dto/response/ResaleListingMyPageCountResponse.java
@@ -0,0 +1,12 @@
+package com.goti.resale.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record ResaleListingMyPageCountResponse(
+	@Schema(description = "판매 중인 갯수", example = "5")
+	Long listingCount,
+
+	@Schema(description = "판매 완료 갯수", example = "5")
+	Long soldCount
+) {
+}
