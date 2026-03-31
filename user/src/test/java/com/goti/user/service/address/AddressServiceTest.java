be27981412d7887dc4bdd6bc2cd7dba4991fab0b
junsang-ee <dkwnstkddk@gmail.com>
2026-04-01 01:04:41 +0900

    refactor: #380
    회원 주소 등록 응답 DTO명 변경 (AddressCreateResponse -> AddressRegisterResponse)

--- user/src/test/java/com/goti/user/service/address/AddressServiceTest.java
+++ user/src/test/java/com/goti/user/service/address/AddressServiceTest.java
@@ -5,7 +5,7 @@ import com.goti.user.GotiUserApplication;
 
 import com.goti.user.domain.entity.user.AddressEntity;
 import com.goti.user.domain.entity.user.MemberEntity;
-import com.goti.user.dto.response.AddressCreateResponse;
+import com.goti.user.dto.response.AddressRegisterResponse;
 import com.goti.user.repository.AddressRepository;
 import com.goti.user.repository.MemberRepository;
 import com.goti.user.service.domain.address.AddressService;
@@ -48,7 +48,7 @@ public class AddressServiceTest {
 
 	@Test
 	void 주소_생성_성공() {
-		AddressCreateResponse response = addressService.register(
+		AddressRegisterResponse response = addressService.register(
 			"06111",
 			"서울특별시 강남구 학동로 343",
 			"(논현동, 포바강남타워) 4층, 15층",
@@ -73,7 +73,7 @@ public class AddressServiceTest {
 		);
 		addressRepository.save(address);
 
-		AddressCreateResponse response = addressService.register(
+		AddressRegisterResponse response = addressService.register(
 			"123123",
 			"서울특별시 관악구 낙성대역 8길 50-4",
 			"104호",
