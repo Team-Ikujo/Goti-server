708ffe2ce24a899ed5a980241dbc4044c52a9e38
suyeon89 <whtndus0809@naver.com>
2026-04-01 15:33:03 +0900

    refactor: #347
    메서드명 변경

--- resale/src/main/java/com/goti/resale/controller/ResaleOrderController.java
+++ resale/src/main/java/com/goti/resale/controller/ResaleOrderController.java
@@ -97,7 +97,7 @@ public class ResaleOrderController {
 		description = "payment 모듈에서 리셀 구매 내역 목록 조회 API"
 	)
 	@GetMapping("/orders/purchases")
-	public ResponseEntity<ApiSuccessResponse<List<ResalePurchaseListResponse>>> getMyPurchaseOrders(
+	public ResponseEntity<ApiSuccessResponse<List<ResalePurchaseListResponse>>> getPurchasesByMember(
 		@RequestParam UUID buyerId,
 		@ParameterObject ResaleOrderPeriodFilterRequest request
 	) {
