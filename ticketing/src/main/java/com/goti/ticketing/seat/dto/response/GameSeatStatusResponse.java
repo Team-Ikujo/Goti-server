ada7fceba86d3ffb2ef28cdef785278489097a36
suyeon89 <whtndus0809@naver.com>
2026-04-03 00:11:29 +0900

    refactor: #406
    좌석 상태 응답 데이터에 좌석 정보 추가

--- ticketing/src/main/java/com/goti/ticketing/seat/dto/response/GameSeatStatusResponse.java
+++ ticketing/src/main/java/com/goti/ticketing/seat/dto/response/GameSeatStatusResponse.java
@@ -11,12 +11,24 @@ public record GameSeatStatusResponse(
 	@Schema(description = "좌석 ID", example = "44444444-4444-4444-4444-444444444444")
 	UUID seatId,
 
+	@Schema(description = "좌석 구역 ID", example = "33333333-3333-3333-3333-333333333333")
+	UUID sectionId,
+
+	@Schema(description = "행 이름", example = "A")
+	String rowName,
+
+	@Schema(description = "좌석 번호", example = "12")
+	Integer seatNum,
+
 	@Schema(description = "좌석 상태", example = "AVAILABLE")
 	SeatStatus status
 ) {
 	public static GameSeatStatusResponse from(SeatStatusEntity seatStatus) {
 		return new GameSeatStatusResponse(
 			seatStatus.getSeat().getId(),
+			seatStatus.getSeat().getSeatSection().getId(),
+			seatStatus.getSeat().getRowName(),
+			seatStatus.getSeat().getSeatNum(),
 			seatStatus.getStatus()
 		);
 	}
