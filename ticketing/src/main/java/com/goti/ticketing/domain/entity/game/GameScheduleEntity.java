48e4f244da0f9aaeb88a6441bdd70e5a3210f674
junsang-ee <dkwnstkddk@gmail.com>
2026-04-02 16:36:56 +0900

    bug: #399
    GameScheduleEntity -> Index 복합인덱스로 수정 (정렬 및 범위 검색 최적화 + 팀 ID를 포함시켜 커버링 인덱스 유도)

--- ticketing/src/main/java/com/goti/ticketing/domain/entity/game/GameScheduleEntity.java
+++ ticketing/src/main/java/com/goti/ticketing/domain/entity/game/GameScheduleEntity.java
@@ -20,16 +20,15 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static lombok.AccessLevel.*;
+import static lombok.AccessLevel.PROTECTED;
 
 @Getter
 @Entity
 @Table(
 	name = "game_schedules",
 	indexes = {
-		@Index(name = "idx_game_start_at", columnList = "start_at"),
-		@Index(name = "idx_game_home_team", columnList = "home_team_id"),
-		@Index(name = "idx_game_away_team", columnList = "away_team_id")
+		@Index(name = "idx_game_search_home", columnList = "start_at, home_team_id"),
+		@Index(name = "idx_game_search_away", columnList = "start_at, away_team_id")
 	}
 )
 @NoArgsConstructor(access = PROTECTED)
