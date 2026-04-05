24022640863c00e1e80b7ae5eeea882553674bb5
suyeon89 <whtndus0809@naver.com>
2026-04-06 02:29:22 +0900

    refactor: #406
    좌석 상태 조회 시 sectionId 제거

--- ticketing/src/main/java/com/goti/ticketing/seat/dto/response/GameSeatStatusResponse.java
+++ ticketing/src/main/java/com/goti/ticketing/seat/dto/response/GameSeatStatusResponse.java
@@ -11,9 +11,6 @@ public record GameSeatStatusResponse(
 	@Schema(description = "좌석 ID", example = "44444444-4444-4444-4444-444444444444")
 	UUID seatId,
 
-	@Schema(description = "좌석 구역 ID", example = "33333333-3333-3333-3333-333333333333")
-	UUID sectionId,
-
 	@Schema(description = "행 이름", example = "A")
 	String rowName,
 
@@ -26,7 +23,6 @@ public record GameSeatStatusResponse(
 	public static GameSeatStatusResponse from(SeatStatusEntity seatStatus) {
 		return new GameSeatStatusResponse(
 			seatStatus.getSeat().getId(),
-			seatStatus.getSeat().getSeatSection().getId(),
 			seatStatus.getSeat().getRowName(),
 			seatStatus.getSeat().getSeatNum(),
 			seatStatus.getStatus()
