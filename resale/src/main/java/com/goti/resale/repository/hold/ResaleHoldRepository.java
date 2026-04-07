a871e6d912925c11d7fc9312c90af1e90fc148e4
AkiStory <sonsungjeon@gmail.com>
2026-04-07 22:46:24 +0900

    refactor : #421 N+1 문제 리팩토링

--- resale/src/main/java/com/goti/resale/repository/hold/ResaleHoldRepository.java
+++ resale/src/main/java/com/goti/resale/repository/hold/ResaleHoldRepository.java
@@ -3,6 +3,7 @@ package com.goti.resale.repository.hold;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.goti.resale.constants.ResaleHoldStatus;
@@ -10,7 +11,12 @@ import com.goti.resale.domain.entity.resale.ResaleHoldEntity;
 
 public interface ResaleHoldRepository extends JpaRepository<ResaleHoldEntity, UUID>, ResaleHoldRepositoryCustom {
 
-	List<ResaleHoldEntity> findAllByIdInAndUserIdAndStatus(List<UUID> ids, UUID userId, ResaleHoldStatus status);
+	@EntityGraph(attributePaths = {"resaleListing"})
+	List<ResaleHoldEntity> findAllByIdInAndUserIdAndStatus(
+		List<UUID> ids,
+		UUID userId,
+		ResaleHoldStatus status
+	);
 
 }
 
